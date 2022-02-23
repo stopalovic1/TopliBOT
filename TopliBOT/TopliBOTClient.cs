a17fc78bdb09e334f4583999bd37467ece093be3
Senad Topalović <stopalovic1@etf.unsa.ba>
2022-02-23 14:19:28 +0100

    Added automatic start of lavalink using processes

--- TopliBOT/TopliBOTClient.cs
+++ TopliBOT/TopliBOTClient.cs
@@ -2,6 +2,7 @@
 using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
 using TopliBOT.Helpers;
 using Victoria;
 
@@ -21,12 +22,12 @@ namespace TopliBOT
 
         public async Task InitializeAsync()
         {
+            await StartLavalinkAsync();
             var token = await File.ReadAllTextAsync(AppDomain.CurrentDomain.BaseDirectory + "/Tokens/botToken.txt");
 
             await _socketClient.LoginAsync(TokenType.Bot, token);
             await _socketClient.StartAsync();
             _socketClient.Log += Log;
-
 
             _serviceProvider = SetupServices();
             _commandHandler = new CommandHandler(_socketClient, _commandService, _serviceProvider);
@@ -41,7 +42,21 @@ namespace TopliBOT
             return Task.CompletedTask;
         }
 
+        private static async Task StartLavalinkAsync()
+        {
+            var process = new ProcessStartInfo
+            {
+                FileName = "java",
+                Arguments = $"-jar \"{Path.Combine(AppContext.BaseDirectory, "Lavalink")}/Lavalink.jar\"",
+                WorkingDirectory = Path.Combine(AppContext.BaseDirectory, "Lavalink"),
+                UseShellExecute = true,
+                CreateNoWindow = false,
+                WindowStyle = ProcessWindowStyle.Minimized
+            };
 
+            Process.Start(process);
+            await Task.Delay(2000);
+        }
 
         private IServiceProvider SetupServices()
                 => new ServiceCollection()
